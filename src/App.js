7b87ac27da5cda16a244051f4840b72822f3a500
Rajdeepkr1 <76088245+Rajdeepkr1@users.noreply.github.com>
2021-04-19 21:58:05 +0530

    boost booster

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <h2>i am from Boster class repo</h2>
+
+      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello CodeSandbox</h1>
+      <h2>i am from Boster class repo</h2>
+
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
